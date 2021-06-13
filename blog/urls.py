0481e6ca4922ead20dc442ceb68a26dc21fbe44a
Salty-dev <saltydevelopment@protonmail.com>
2021-06-13 05:47:31 +0200

    Design to Post page

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import blog
+from blog.views import blog, post
 
 urlpatterns = [
     path('', blog, name='blog'),
+    path('post', post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def blog(request):
     context = {}
     return render(request, 'blog/blog.html', context)
+
+def post(request):
+    context = {}
+    return render(request, 'blog/post.html', context)
